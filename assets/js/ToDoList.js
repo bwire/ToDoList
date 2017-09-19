4bb1c4ccdd989362b5dffb552cddb194de4e7b9d
BarbedWire <barbedwire@BarbedWires-MacBook-Pro.local>
2017-09-19 14:34:36 +0300

    Stage 5. Deleting elements

--- assets/js/ToDoList.js
+++ assets/js/ToDoList.js
@@ -1,3 +1,10 @@
 $("li").click(function() {
   $(this).toggleClass("done");
+});
+
+$("span").on("click", function(e) {
+  $(this).parent().fadeOut(500, function() {
+    $(this).remove();  
+  });
+  e.stopPropagation();
 })
